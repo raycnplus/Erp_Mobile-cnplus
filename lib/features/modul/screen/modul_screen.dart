969637e01dc487ed6d1554b48bd4eafafc5a3eb5
AhdMaul <a.maulana.cnplus@gmail.com>
2025-07-25 10:53:52 +0700

    Fix gradient color in DashboardScreen background

--- lib/features/modul/screen/modul_screen.dart
+++ lib/features/modul/screen/modul_screen.dart
@@ -23,7 +23,7 @@ class DashboardScreen extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFFE7F8F2), Color.fromARGB(255, 27, 99, 51)],
+            colors: [Color(0xFFE7F8F2), Color.(0xFFABE6C1)],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
