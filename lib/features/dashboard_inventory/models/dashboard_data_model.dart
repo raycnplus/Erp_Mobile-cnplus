abfa8febdd768ffd853be2c78c03d1325d77d0e2
AhdMaul <a.maulana.cnplus@gmail.com>
2025-07-25 09:03:14 +0700

    Add DashboardData model and related widgets for inventory dashboard nambahin methode save token ke lokal

---
+++ lib/features/dashboard_inventory/models/dashboard_data_model.dart
@@ -0,0 +1,13 @@
+class DashboardData {
+  final int receiptNote;
+  final int deliveryNote;
+  final int lowStockAlert;
+  final int onHandStock;
+
+  DashboardData({
+    required this.receiptNote,
+    required this.deliveryNote,
+    required this.lowStockAlert,
+    required this.onHandStock,
+  });
+}
