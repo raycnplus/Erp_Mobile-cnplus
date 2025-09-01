036a4f0d625efeeed1f1c7d18458264b621dbed5
AhdMaul <a.maulana.cnplus@gmail.com>
2025-09-01 11:13:21 +0700

    fix error interval pada sidetitles package fl_chart

--- lib/features/dashboard_inventory/screen/dashboard_inventory.dart
+++ lib/features/dashboard_inventory/screen/dashboard_inventory.dart
@@ -87,7 +87,7 @@ class _DashboardInventoryScreenState extends State<DashboardInventoryScreen> {
             Navigator.pushNamed(context, AppRoutes.modul);
           },
           child: const Text(
-            'Inventory',
+            'Inventory Dashboard',
             style: TextStyle(color: Colors.black),
           ),
         ),
